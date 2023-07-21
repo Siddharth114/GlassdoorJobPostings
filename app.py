ce4b8956e5548e56f984066bc00138a775e6bc2f
Siddharth <2004sid@gmail.com>
2023-07-21 10:37:09 +0530

    started streamlit dashboard

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-import plotly.express as px  # (version 4.7.0 or higher)
-import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
+import streamlit as st
+from streamlit_option_menu import option_menu
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('DS_jobs.csv')
 df['Location'] = df['Location'].replace({'United States': 'San Francisco, CA', 'Patuxent, Anne Arundel, MD':'Anne Arundel, MD'})
@@ -16,70 +17,64 @@ df['aws'] = df['aws'].replace({0: np.nan, 1: 'aws'})
 df['tableau'] = df['tableau'].replace({0: np.nan, 1: 'yableau'})
 df['big_data'] = df['big_data'].replace({0: np.nan, 1: 'big_data'})
 
-sector_grouped_df = df.groupby('Sector')['Industry'].value_counts().sort_values(ascending=False)
+overall_avg_salary = round(sum(df['avg_salary'])/len(df['avg_salary']), 2)
+overall_min_salary = round(sum(df['min_salary'])/len(df['min_salary']), 2)
+overall_max_salary = round(sum(df['max_salary'])/len(df['max_salary']), 2)
+
+st.set_page_config(layout='wide')
+
+def main():
+
+    nav = option_menu(None, ["Sector-Wise", 'State-Wise'], 
+        icons=['briefcase', 'geo-alt'], default_index=0, menu_icon='list', orientation='horizontal')
+    
+    if nav=='Sector-Wise':
+        
+       
+
+        sector_select = st.selectbox('Filter by Sector', ('All sectors', 'Insurance', 'Business Services', 'Manufacturing',
+       'Information Technology', 'Biotech & Pharmaceuticals', 'Retail',
+       'Oil, Gas, Energy & Utilities', 'Government', 'Health Care',
+       'Finance', 'Aerospace & Defense', '-1',
+       'Transportation & Logistics', 'Media', 'Telecommunications',
+       'Real Estate', 'Travel & Tourism', 'Agriculture & Forestry',
+       'Education', 'Accounting & Legal', 'Non-Profit',
+       'Construction, Repair & Maintenance', 'Consumer Services'))
+        
+        if sector_select == 'All sectors':
+            sectors_df = df.groupby('Sector')['Industry'].value_counts().sort_values(ascending=False).reset_index(name='Number of Job Postings')
+            sectors_df = sectors_df[['Sector', 'Number of Job Postings']]
+            agg_functions = {'Number of Job Postings':'sum'}
+            sectors_df = sectors_df.groupby(sectors_df['Sector']).aggregate(agg_functions).reset_index()
+            st.bar_chart(data=sectors_df, x='Sector', y='Number of Job Postings')
+
+            col1, col2, col3 = st.columns(3)
+            col1.metric('Overall Average Salary (in thousands of dollars)',f'{overall_avg_salary}')
+            col2.metric('Overall Average Starting Salary (in thousands of dollars)',f'{overall_min_salary}')
+            col3.metric('Overall Average Maximum Salary (in thousands of dollars)',f'{overall_max_salary}')
 
 
-app = Dash(__name__)
+        else:
+            sector_filter_df = df[df['Sector']==sector_select]['Industry']
+            sector_filter_df = sector_filter_df.reset_index()
+            sector_filter_df.drop(columns=('index'), inplace=True)
+            sector_filter_df = sector_filter_df.groupby('Industry').size().reset_index(name='Number of Job Postings')
+            st.bar_chart(data=sector_filter_df, x='Industry', y='Number of Job Postings')
 
-app.layout = html.Div([
-    html.H1("Glass Door Job Postings", style={'text-align': 'center'}),
+            sector_filter_df_copy = df[df['Sector']==sector_select]
+            sector_avg_salary = round(sum(sector_filter_df_copy['avg_salary'])/len(sector_filter_df_copy['avg_salary']), 2)
+            sector_min_salary = round(sum(sector_filter_df_copy['min_salary'])/len(sector_filter_df_copy['min_salary']), 2)
+            sector_max_salary = round(sum(sector_filter_df_copy['max_salary'])/len(sector_filter_df_copy['max_salary']), 2)
 
-    dcc.Dropdown(id='slct_sector',
-                 options = [
-                     {'label':'Information Technology', 'value':'Information Technology'},
-                     {'label':'Business Services', 'value':'Business Services'},
-                     {'label':'Manufacturing', 'value':'Manufacturing'},
-                     {'label':'Insurance', 'value':'Insurance'},
-                     {'label':'Biotech & Pharmaceuticals', 'value':'Biotech & Pharmaceuticals'},
-                     {'label':'Retail', 'value':'Retail'},
-                     {'label':'Oil, Gas, Energy & Utilities', 'value':'Oil, Gas, Energy & Utilities'},
-                     {'label':'Government', 'value':'Government'},
-                     {'label':'Health Care', 'value':'Health Care'},
-                     {'label':'Finance', 'value':'Finance'},
-                     {'label':'Aerospace & Defense', 'value':'Aerospace & Defense'},
-                     {'label':'Unorganized', 'value':'-1'},
-                     {'label':'Transportation & Logistics', 'value':'Transportation & Logistics'},
-                     {'label':'Media', 'value':'Media'},
-                     {'label':'Telecommunications', 'value':'Telecommunications'},
-                     {'label':'Real Estate', 'value':'Real Estate'},
-                     {'label':'Travel & Tourism', 'value':'Travel & Tourism'},
-                     {'label':'Agriculture & Forestry', 'value':'Agriculture & Forestry'},
-                     {'label':'Education', 'value':'Education'},
-                     {'label':'Accounting & Legal', 'value':'Accounting & Legal'},
-                     {'label':'Non-Profit', 'value':'Non-Profit'},
-                     {'label':'Construction, Repair & Maintenance', 'value':'Construction, Repair & Maintenance'},
-                     {'label':'Consumer Services', 'value':'Consumer Services'},
-                 ],
-                 multi=False,
-                 value='Insurance',
-                 style = {'width':'40%'}
-    ),
+            col1, col2, col3 = st.columns(3)
+            col1.metric('Sector Average Salary (in thousands of dollars)',f'{sector_avg_salary}', round(sector_avg_salary-overall_avg_salary, 2))
+            col2.metric('Sector Average Starting Salary (in thousands of dollars)',f'{sector_min_salary}', round(sector_min_salary-overall_min_salary, 2))
+            col3.metric('Sector Average Maximum Salary (in thousands of dollars)',f'{sector_max_salary}', round(sector_max_salary-overall_max_salary, 2))
 
-    html.Div(id='output_container', children=[]),
-    html.Br(),
 
-    dcc.Graph(id='my_bee_map', figure={})
-])
 
-@app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='my_bee_map', component_property='figure')],
-     [Input(component_id='slct_sector', component_property='value')]
-)
-def update_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
 
-    container = "The sector chosen by the user was: {}".format(option_slctd)
 
-    dff = df.copy()
-    dff = dff[dff["Sector"] == option_slctd]
-    fig = px.histogram(
-        dff,
-        x='Industry',
-    )
-
-    return container, fig
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    
+if __name__=='__main__':
+    main()
