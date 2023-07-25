e69940f2bb304dd696e74cb7713fff551987d7ee
Siddharth <2004sid@gmail.com>
2023-07-25 09:34:08 +0530

    state filter page

--- app.py
+++ app.py
@@ -28,6 +28,48 @@ overall_avg_salary = round(sum(df["avg_salary"]) / len(df["avg_salary"]), 2)
 overall_min_salary = round(sum(df["min_salary"]) / len(df["min_salary"]), 2)
 overall_max_salary = round(sum(df["max_salary"]) / len(df["max_salary"]), 2)
 
+state_codes = {
+    "New York": "NY",
+    "Virginia": "VA",
+    "Massachusetts": "MA",
+    "California": "CA",
+    "Illinois": "IL",
+    "Missouri": "MO",
+    "Washington": "WA",
+    "District of Columbia": "DC",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Pennsylvania": "PA",
+    "Arizona": "AZ",
+    "Wisconsin": "WI",
+    "Georgia": "GA",
+    "Florida": "FL",
+    "United States": "US",
+    "Nebraska": "NE",
+    "Kansas": "KS",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "Louisiana": "LA",
+    "Ohio": "OH",
+    "Indiana": "IN",
+    "Maryland": "MD",
+    "Colorado": "CO",
+    "Utah": "UT",
+    "Oregon": "OR",
+    "Michigan": "MI",
+    "South Carolina": "SC",
+    "Mississippi": "MS",
+    "Alabama": "AL",
+    "Rhode Island": "RI",
+    "Iowa": "IA",
+    "Minnesota": "MN",
+    "Oklahoma": "OK",
+    "Connecticut": "CT",
+    "North Carolina": "NC",
+    "Delaware": "DE",
+    "West Virginia": "WV",
+}
+
 st.set_page_config(layout="wide")
 
 
@@ -86,7 +128,7 @@ def main():
                 .aggregate(agg_functions)
                 .reset_index()
             )
-            st.subheader("Number of Job Postings per Industry")
+            st.subheader("Number of Job Postings by Industry")
             fig = px.bar(
                 sectors_df,
                 x="Sector",
@@ -254,7 +296,7 @@ def main():
                 .size()
                 .reset_index(name="Number of Job Postings")
             )
-            st.subheader("Number of Job Postings per Industry")
+            st.subheader("Number of Job Postings by Industry")
             fig = px.bar(
                 sector_filter_df,
                 x="Industry",
@@ -444,9 +486,207 @@ def main():
             st.subheader("State Heatmap")
             st.plotly_chart(fig, use_container_width=True)
 
-    elif nav=='PygWalker Exploration':
+    elif nav == "PygWalker Exploration":
         pyg_html = pyg.walk(df, return_html=True)
         components.html(pyg_html, height=1000, scrolling=True)
+
+    else:
+        state_select = st.sidebar.selectbox(
+            "",
+            (
+                "All States",
+                "New York",
+                "Virginia",
+                "Massachusetts",
+                "California",
+                "Illinois",
+                "Missouri",
+                "Washington",
+                "District of Columbia",
+                "Tennessee",
+                "Texas",
+                "Pennsylvania",
+                "Arizona",
+                "Wisconsin",
+                "Georgia",
+                "Florida",
+                "United States",
+                "Nebraska",
+                "Kansas",
+                "New Hampshire",
+                "New Jersey",
+                "Louisiana",
+                "Ohio",
+                "Indiana",
+                "Maryland",
+                "Colorado",
+                "Utah",
+                "Oregon",
+                "Michigan",
+                "South Carolina",
+                "Mississippi",
+                "Alabama",
+                "Rhode Island",
+                "Iowa",
+                "Minnesota",
+                "Oklahoma",
+                "Connecticut",
+                "North Carolina",
+                "Delaware",
+                "West Virginia",
+            ),
+        )
+
+        if state_select != "All States":
+            state_df = df[df["job_state"] == state_codes[state_select]]
+        else:
+            state_df = df
+
+        sectors_df = (
+            state_df.groupby("Sector")["Industry"]
+            .value_counts()
+            .sort_values(ascending=False)
+            .reset_index(name="Number of Job Postings")
+        )
+        sectors_df = sectors_df[["Sector", "Number of Job Postings"]]
+        agg_functions = {"Number of Job Postings": "sum"}
+        sectors_df = (
+            sectors_df.groupby(sectors_df["Sector"])
+            .aggregate(agg_functions)
+            .reset_index()
+        )
+        st.subheader("Number of Job Postings by Sector")
+        fig = px.bar(
+            sectors_df,
+            x="Sector",
+            y="Number of Job Postings",
+            color_discrete_sequence=px.colors.qualitative.Antique,
+        )
+        st.plotly_chart(fig, use_container_width=True)
+        st.divider()
+
+        if state_select=='All States':
+            st.subheader("Overall Salary Metrics (in thousands of dollars)")
+            col1, col2, col3 = st.columns(3)
+            col1.metric(
+                "Average Salary",
+                f"{overall_avg_salary}",
+            )
+            col2.metric(
+                "Average Starting Salary",
+                f"{overall_min_salary}",
+            )
+            col3.metric(
+                "Average Maximum Salary",
+                f"{overall_max_salary}",
+            )
+
+            style_metric_cards(border_left_color="#4682A9", background_color="#212A3E")
+            st.divider()
+        else:
+            st.subheader('Salary for Chosen State vs. Overall Salary Metrics (in thousands of dollars)')
+            col1, col2, col3 = st.columns(3)
+            state_avg_salary = round(
+                sum(state_df['avg_salary'])/len(state_df['avg_salary']), 2
+            )
+            state_min_salary = round(
+                sum(state_df['min_salary'])/len(state_df['min_salary']), 2
+            )
+            state_max_salary = round(
+                sum(state_df['max_salary'])/len(state_df['max_salary']), 2
+            )
+            col1.metric(
+                "Average Salary",
+                f"{state_avg_salary}",
+                round(state_avg_salary - overall_avg_salary, 2),
+            )
+            col2.metric(
+                "Average Starting Salary",
+                f"{state_min_salary}",
+                round(state_min_salary - overall_min_salary, 2),
+            )
+            col3.metric(
+                "Average Maximum Salary",
+                f"{state_max_salary}",
+                round(state_max_salary - overall_max_salary, 2),
+            )
+            style_metric_cards(border_left_color="#4682A9", background_color="#212A3E")
+            st.divider()
+
+        col1, col2 = st.columns(2)
+        skill_counts = {}
+        skills = [
+            "python",
+            "excel",
+            "hadoop",
+            "spark",
+            "aws",
+            "tableau",
+            "big_data",
+        ]
+        for skill in skills:
+            try:
+                skill_counts[skill.capitalize()] = state_df[
+                    skill
+                ].value_counts()[skill]
+            except:
+                skill_counts[skill.capitalize()] = 0
+        
+        state_skills_df = pd.DataFrame.from_dict(
+            skill_counts, orient="index", columns=["Number of Job Postings"]
+        )
+        state_skills_df = state_skills_df.reset_index()
+        state_skills_df.rename(columns={'index':'Skill'}, inplace=True)
+        fig = px.pie(
+            state_skills_df,
+            values="Number of Job Postings",
+            names="Skill",
+            hole=0.4,
+            color_discrete_sequence=px.colors.qualitative.Antique,
+        )
+        col1.subheader("Skill Distribution")
+        col1.plotly_chart(fig, use_container_width=True)
+
+        title_counts = {}
+        titles = state_df["job_simp"].unique()
+        for title in titles:
+            if title == "Unspecified":
+                continue
+            title_counts[title.capitalize()] = state_df[
+                "job_simp"
+            ].value_counts()[title]
+
+        titles_df = pd.DataFrame.from_dict(
+            title_counts, orient="index", columns=["Number of Job Postings"]
+        )
+        titles_df = titles_df.reset_index()
+        titles_df.rename(columns={"index": "Job Title"}, inplace=True)
+        fig = px.pie(
+            titles_df,
+            values="Number of Job Postings",
+            names="Job Title",
+            hole=0.4,
+            color_discrete_sequence=px.colors.qualitative.Antique,
+        )
+        col2.subheader("Job Title Distribution")
+        col2.plotly_chart(fig, use_container_width=True)
+
+
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
